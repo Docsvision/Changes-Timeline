6d438352bc4768fc4dddc111ec43e0ee74d869e5
Volkov Alexander <volkov.525@gmail.com>
2024-06-25 14:17:39 +0300

    add quasar

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [
+		vue({
+			template: { transformAssetUrls },
+		}),
+		vueDevTools(),
+		quasar({
+			sassVariables: 'src/quasar-variables.scss',
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
 })
